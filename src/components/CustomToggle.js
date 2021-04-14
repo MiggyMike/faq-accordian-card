2c5ff4da2e84116f53c8cb8669167922c003195a
MICHAEL <MJENNINGS6@gmail.com>
2021-04-14 14:28:48 -0400

    updated for arrow icon functionality

--- src/components/CustomToggle.js
+++ src/components/CustomToggle.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { useAccordionToggle, AccordionContext, Card } from 'react-bootstrap';
 import '../styles/_card.scss';
+import { ReactComponent as IconSvg } from '../images/icon-arrow-down.svg';
 
 const CustomToggle = ({ children, eventKey, callback }) => {
     const currentEventKey = useContext(AccordionContext);
@@ -15,9 +16,19 @@ const CustomToggle = ({ children, eventKey, callback }) => {
     return (
         <Card.Header
             type='link'
-            style={{ fontWeight: isCurrentEventKey ? 'bold' : '' }}
+            style={{
+                fontWeight: isCurrentEventKey ? 'bold' : '',
+            }}
             onClick={decoratedOnClick}>
             {children}
+            <div
+                className='icon'
+                style={{
+                    transform: isCurrentEventKey ? ' rotate(180deg)' : '',
+                }}
+                onClick={decoratedOnClick}>
+                {<IconSvg />}
+            </div>
         </Card.Header>
     );
 };
